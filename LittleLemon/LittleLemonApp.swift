3b54bc193f429b55301492e64449a0013de5559f
DhaiAssaf <dhasaf@icloud.com>
2024-05-22 10:00:17 +0300

    Initial Commit

---
+++ LittleLemon/LittleLemonApp.swift
@@ -0,0 +1,17 @@
+//
+//  LittleLemonApp.swift
+//  LittleLemon
+//
+//  Created by Dhai Alassaf on 22/05/2024.
+//
+
+import SwiftUI
+
+@main
+struct LittleLemonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
